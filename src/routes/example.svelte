18bc3e0d6e8c39f73230f7c9d1ea04966cddb9cf
Joe Harlow <joe@f5.io>
2022-04-01 01:19:15 +0100

    cloudflare prerender bug

---
+++ src/routes/example.svelte
@@ -0,0 +1,5 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<h1>If you refresh me, I go boom!</h1>
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<h1>Root works fine</h1>
+<a href="/example">Example</a>
